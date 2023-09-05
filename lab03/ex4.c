38e6e69989cdd6951148dd5fef88588997bb8474
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-09-05 10:51:32 +0500

    Create ex4.c

---
+++ lab03/ex4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int saleprice;
+	int a;
+	printf("Enter Original price:");
+	scanf("%d", &a);
+
+	int b;
+	printf("Enter discount:");
+	scanf("%d", &b);
+	
+	float saleprice; (a/10)*(b/10); 
+	printf("%.2f", saleprice);
+
+}
