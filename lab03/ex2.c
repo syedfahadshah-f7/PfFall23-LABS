6b3555c327450a6cba44d57f6abaa09a4dbb3f8f
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-09-05 10:48:43 +0500

    Create ex2.c

---
+++ lab03/ex2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("Enter the height\n");
+	scanf("%d", &a);
+
+	int b;
+	printf("Enter the length\n");
+	scanf("%d", &b);
+
+	int c;
+	printf("Enter the width\n");
+	scanf("%d", &c);
+	printf("Volume = %d", a*b*c);
+return 0;	
