9984e605cf18208414dc52196c19cdc277ede6f7
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-09-12 10:37:49 +0500

    Update task03.c

--- lab04/task03.c
+++ lab04/task03.c
@@ -1,1 +1,29 @@
+/*programmer : Syed Fahad Faheem shah
+ *Date:9-12-2023
+ *Description:This is a program for print sum of the squares of integers.
+*/
+#include<stdio.h>
 
+int main(){
+	int num1;
+	printf("Enter your number");
+	scanf("%d", num);
+
+	int squr;
+	int final;
+	int a= 0;
+	int i = 0;
+	for(i<=num1){
+	int i= i++;
+	int x;
+	printf("Enter your numbers")
+	scanf("%d", &x);
+
+	squr= x * x ;
+	a= squr +a ;
+	printf("%d", a);
+	
+}//end loop
+	
+	return 0;
+}//end main
