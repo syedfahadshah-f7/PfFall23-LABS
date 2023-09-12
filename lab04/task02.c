f33b29e818fe72447346862e7686eec91f71d5c0
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-09-12 18:03:30 +0500

    Update task02.c

--- lab04/task02.c
+++ lab04/task02.c
@@ -1,1 +1,30 @@
+/*Programmer : Syed Fahad Faheem shah
+ *Date:9-12-2023
+ *Description:This is a program for printing stars acccording to user input.
+ */
+#include <stdio.h>
 
+int main() {
+    int n;
+    printf("Enter Your Number:");// put number you want to print stars in lines.
+    scanf("%d", &n);
+    
+    int i = 0;
+    while(i < n){
+        i =i + 1;
+        
+        int j = 0;
+        while(j < n){
+           printf(" * ");
+            j = j + 1;
+            
+        }//end loop
+        
+        printf("\n");
+        
+    }//end main loop
+    
+    
+
+    return 0;
+}// end main
