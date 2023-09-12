3b1059aeb3bfe180f638456199acdf0103adbaf1
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-09-12 19:36:26 +0500

    Update task04.c

--- lab04/task04.c
+++ lab04/task04.c
@@ -1,1 +1,46 @@
 
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a,b,c;
+    	printf("Enter valueof a:");
+    	scanf("%lf", &a);
+	printf("Enter value of b:");
+    	scanf("%lf", &b);
+	printf("Enter value of c:");
+    	scanf("%lf", &c);
+
+	if(a==0){
+	printf("value bill infinty");
+	return 1;
+}
+    
+    double x,y,z;// holding value
+    double radical;
+    double numerator;// holding square of function 
+    double ansone;
+    double anstwo;
+    
+	x = b * b;
+	y = 4*a*c;
+	z = x-y;
+	
+	if(z<0){
+	printf("Roots are complex");
+	return 1;
+}
+
+   radical = sqrt(z);
+
+   numerator = -b - radical;
+   ansone = numerator / (2*a);
+   
+   numerator = -b + radical;
+   anstwo = numerator / (2*a);
+    
+    	printf("%.2lf", ansone);
+	printf("%.2lf", anstwo);
+
+    return 0;
+}// end main
