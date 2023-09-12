4a60d92b6f7f9281441f2a76719d64a1324a5762
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-09-12 17:58:51 +0500

    Update task1.c

--- lab04/task1.c
+++ lab04/task1.c
@@ -2,27 +2,29 @@
  *Date:9-12-2023
  *Description:This is a program for printing stars acccording to user input.
  */
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-	int num;
-	printf("Enter your number");
-	scanf("%d", &num);
+int main() {
+    int n;
+    printf("Enter Your Number:");// put number you want to print stars.
+    scanf("%d", &n);
+    
+    int i = 0;
+    while(i < n){
+        i =i + 1;
+        
+        int j = 0;
+        while(j < i){
+           printf(" * ");
+            j = j + 1;
+            
+        }//end loop
+        
+        printf("\n");
+        
+    }//end main loop
+    
+    
 
-	int a=1 ;
-	int b=0 ;
-
-	while(a <= num)
-	{
-		printf("*");
-		a= a+1 ;
-	}// end loop
-	
-	while(b <= a){
-	printf("*\n");
-	b=b+1;	
-
-}// end loop
-	return 0;
-
-}//end main
+    return 0;
+}// end main
