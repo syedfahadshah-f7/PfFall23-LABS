b2658956e7df81247e20fcc35b90c99ca7ab1ca5
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-09-19 20:49:43 +0500

    Update q03.c

--- lab05/q03.c
+++ lab05/q03.c
@@ -1,3 +1,7 @@
+/*Programmer : Syed Fahad Faheem shah (23k-0062)
+ *Date:9-19-2023
+ *Description:This is a program for asking you sure to delete or not.
+ */
 #include<stdio.h>
 void main (){
 	char hold;
