7be614419a986e4c52b9eeda8acf5a8d122d1b4c
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-09-19 09:59:08 +0500

    Create q06.c

---
+++ lab05/q06.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main(){
+	int num,check;
+	printf("Enter *bit number");
+	scanf("%d", &num);
+	
+	 if(check = num / 1000 && num / 1000000;){
+
+	check = check - 1000 -1000000;
+ 
+	
+}// end else if
+	printf("%c", check);
+
+}// end main
