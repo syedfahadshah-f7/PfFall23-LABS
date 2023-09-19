5580207b60ee141e1d990fd55152bea1592f15d3
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-09-19 23:25:20 +0500

    Create task06.c

---
+++ lab04/task06.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a value for n: ");
+    scanf("%d", &n);
+
+    // Print upper part of the pattern
+    int i = 1;
+    while (i <= n) {
+        int j = 1;
+        while (j <= n - i) {
+            printf(" ");
+            j++;
+        }//end loop
+
+        printf("*");
+
+        if (i > 1) {
+            j = 1;
+            while (j <= 2 * (i - 1) - 1) {
+                printf(" ");
+                j++;
+            }//end loop
+            printf("*");
+        }//end if
+
+        printf("\n");
+        i++;
+    }//wmd main upper loop
+
+    // Print lower part of the pattern
+    i = n - 1;
+    while (i >= 1) {
+        int j = 1;
+        while (j <= n - i) {
+            printf(" ");
+            j++;
+        }// end loop
+        printf("*");
+
+        if (i > 1) {
+            j = 1;
+            while (j <= 2 * (i - 1) - 1) {
+                printf(" ");
+                j++;
+            }//end loop
+            printf("*");
+        }//end if
+
+        printf("\n");
+        i--;
+    }//end main lower loop
+
+    return 0;
+}//end main
