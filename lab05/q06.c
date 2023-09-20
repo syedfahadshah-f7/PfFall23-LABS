112afe3f371d90d2b7f8142372e87e6bee20c250
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-09-20 15:59:35 +0500

    Update q06.c

--- lab05/q06.c
+++ lab05/q06.c
@@ -2,23 +2,20 @@
  *Date:9-19-2023
  *Description:This is a program for check 4th bit and 7th bit is on of off, if on then off it.
  */
-#include <stdio.h>
-
-int main() {
-    // All are bits values from bit1 to bit 8
-    int one, two, three, four, five, six, seven, eight;
-
-    printf("Enter 8 bits number: ");
-    scanf("%d %d %d %d %d %d %d %d", &one, &two, &three, &four, &five, &six, &seven, &eight);
-
-    if (four != 0 && seven != 0) {
-        four = 0;
-        seven = 0;
-        printf("\n%d %d %d %d %d %d %d %d\n", one, two, three, four, five, six, seven, eight);
-    } //end if
-    else {
-        printf("\n%d %d %d %d %d %d %d %d\n", one, two, three, four, five, six, seven, eight);
-    }// end else
-
-    return 0;
-}//end main
+#include<stdio.h>
+  int main (){
+  int num, bit4, bit7;
+    printf("Enter 8 bit number:");
+    scanf("%d", &num);
+    
+   bit4 = (num /1000) % 10;
+  bit7 = (num /1000000) % 10;
+  
+  if(bit4!=0 && bit7!=0){
+   
+   num = num-1000000-1000;
+  }//end if
+  printf("Final numbers:%.8d", num);
+  return 0;
+  }//end main
+  
