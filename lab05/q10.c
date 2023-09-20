b7c12451088c31a6da73b8d6efe7772f232af678
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-09-20 15:57:50 +0500

    Update q10.c

--- lab05/q10.c
+++ lab05/q10.c
@@ -2,23 +2,18 @@
  *Date:9-19-2023
  *Description:This is a program  to exchange higher four bits with the lower four bits .
  */
-#include<stdio.h>
-void main (){
-	// All are bits values from bit1 to bit 8
-		int one, two, three, four, five, six, seven, eight, i, j, k, l;
+#include <stdio.h>
 
-		printf("Enter 8 bits number:");
-		scanf("%d %d %d %d %d %d %d %d", &one, &two, &three, &four, &five, &six, &seven, &eight);
-
-		i = five;
-		j = six; 
-		k = seven;
-		l = eight;
-		
-		five =one; 
-		six = two; 
-		seven = three; 
-		eight = four;
-
-		printf("your change bits:%d %d %d %d %d %d %d %d \n", i, j, k, l, five, six, seven, eight);
-}// end main
+int main() {
+    int num, i, j;
+    printf("Enter 8 bit number:");
+    scanf("%d", &num);
+    
+    if (num <10){
+    i = num % 10000;
+    j= num / 10000;
+    
+    printf("Swap bits: %.4d%.4d", i, j);
+}//end if
+    return 0;
+}//end main
