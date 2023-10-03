23e09203c9e28e53a5a20feb6f343f5605f288e9
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-10-03 10:25:32 +0500

    Create q3.c

---
+++ lab06/q3.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main() {
+   int num1,num2, check,store,lcm,j=1;
+   printf("Enter Two Numbers:");
+   scanf("%d %d", &num1,&num2);
+   
+   if(num1 < num2){check = num1;}
+   else {check = num2;}
+  
+   for(int j = check;j>0;j--){
+       if(num1%j == 0 && num2%j ==0){
+           if(j>=check){
+        printf("GCD is:%d", j);} 
+        lcm = (num1*num2)/j;
+      printf("\nLCM is:%d", lcm);
+   
+        break ;
+       }// end if
+   }// end for
+    return 0;
+}
