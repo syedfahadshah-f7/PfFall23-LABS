415cc62bd69f3410e377204a4e9157895d4264ca
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-10-03 09:34:17 +0500

    Create q2.c

---
+++ lab06/q2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+int sum=0,fibonachi=1,a=0,b=0;
+
+
+
+for(int i=0;fibonachi<10000;i++){
+
+if(fibonachi%3 == 0 || fibonachi%5 == 0 || fibonachi%7 == 0){
+	sum +=fibonachi;
+}// end if	
+
+
+a=b;
+b=fibonachi;
+fibonachi=a+b;
+printf("%d,", fibonachi);
+
+
+}// end loop
+printf("\nSum is:%d", sum);
+return 0;
+}// end main
