5021264f9723b0eb0a3d4276eaa988de8dc16611
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-10-03 18:14:55 +0500

    Create q9.c

---
+++ lab06/q9.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void main(){
+
+    int  size;
+    printf("Enter Size Of Array:");
+    scanf("%d", &size);
+
+    int arr[size];
+    for(int i =0;i<size;i++){
+        printf("Enter Number%d:", i+1);
+        scanf("%d", &arr[i]);
+    }// end initialization loop
+  
+    int max=arr[0], min=arr[0];
+    for(int i =0;i<size;i++){
+        if(max < arr[i])
+        { max = arr[i];}
+
+        else if(min > arr[i])
+        { min = arr[i];}
+
+    }//end loop
+
+    printf("Maximum Number:%d Minimum Number:%d", max,min);
+}// end main
