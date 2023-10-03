349c9e43d8ba8ed6cecffffb4fbb224e625cbba1
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-10-03 20:29:25 +0500

    Update q6.c

--- lab06/q6.c
+++ lab06/q6.c
@@ -1,4 +1,7 @@
-
+/*Programmer : Syed Fahad Faheem shah (23k-0062)
+ *Date:10-03-2023
+ *Description: program for a match-stick game between the computer and a user.
+   */
 #include <stdio.h>
 
 int main() {
