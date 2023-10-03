6a6358700c44956b9bb3e41907e23ebedac2c533
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-10-03 09:34:46 +0500

    Create q1.c

---
+++ lab06/q1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+int num, sum=0;
+printf("Enter number:");
+scanf("%d", &num);
+
+for(int i=1;i<=num/2;i++){
+if(num%i == 0){
+sum =sum + i;
+}// end if
+}// end loop
+if(num == sum){
+printf("Number is Perfect");
+}// end if
+else{ printf("Number is not perfect");
+}// end else
+}// end main
