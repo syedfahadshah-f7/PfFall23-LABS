96382b82bf262188922621569efc25a3e655dd48
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-10-27 15:28:08 +0500

    Create task11.c

---
+++ lab 07/task11.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+
+void main() {
+    int n,max=0;
+    printf("Enter Grid size:");
+    scanf("%d", &n);
+    printf("Enter Values of Grid\n");
+   int arr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }          
+    
+    int maxlocal[n-2][n-2];
+   int a=3,b=3;
+   
+    for(int i=0;i<n-2;i++){
+        for(int j=0;j<n-2;j++){
+            for(int k=i;k<a;k++){
+              max =0;
+                for(int l=j;l<b;l++){
+                    if(max< arr[k][l]){
+                        max =arr[k][l];
+                    }
+                }// end l loop
+            }// end k loop
+         maxlocal[i][j]=max; 
+         if(b%n ==0) {
+             a++;
+             b=3;
+         }else b++;
+        }// end j loop
+        
+    }// end i loop
+    
+    //printing maxlocal
+    printf(" Values of Maxlocal\n");
+    for(int i=0;i<n-2;i++){
+        for(int j=0;j<n-2;j++){
+            printf("%d\t", maxlocal[i][j]);
+        }
+        printf("\n");
+    }
+}// end main
