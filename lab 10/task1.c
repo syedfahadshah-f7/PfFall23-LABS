cd6aeeac04f39831e97bede4be69600bc1ebbeb6
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-11-15 20:24:59 +0500

    Update task1.c

--- lab 10/task1.c
+++ lab 10/task1.c
@@ -1,44 +1,67 @@
-// Online C compiler to run C program online
+/*
+* Programmer: Syed Fahad Faheem Shah(23k-0062)
+* Date: 11/15/2023
+* Description: Create a universal array and a function that takes a void* as an argument along with size and some way of know what kind of data is stored. Then prints the values stored in the memory location pointed to by the void pointer. Use appropriate casting.
+*/
+
 #include <stdio.h>
-void printingfunction(void* arr,int datatype){
-    
-    if(4==datatype/sizeof(int)){
-        printf("Array data type is int\n");
-        for(int i=0;i<datatype/sizeof(arr[0]);i++){
-            printf("%d", arr+i);
-        }
-    }
-    if(1==datatype/sizeof(char)){
-        printf("Array data type is char\n");
-        for(int i=0;i<datatype/sizeof(arr[0]);i++){
-            printf("%c", arr+i);
-        }
-    }
-    if(8==datatype/sizeof(double)){
-        printf("Array data type is double\n");
-        for(int i=0;i<datatype/sizeof(arr[0]);i++){
-            printf("%lf", arr+i);
-        }
-    }
-    if(4==datatype/sizeof(float) ){
-        printf("Array data type is float\n");
-        for(int i=0;i<datatype/sizeof(arr[0]);i++){
-            printf("%f", arr+i);
-        }
-    }
-}
-    
 
-
-int main() {
-    int intArr[] = {1, 2, 3, 4, 5};
-    float floatArr[] = {1.1, 2.2, 3.3, 4.4, 5.5};
-    double doubleArr[] = {1.111, 2.222, 3.333, 4.444, 5.555};
-    char charArr[]= {'a', 'b', 'c', 'd'} ;
-    
-     printingfunction(intArr,sizeof(intArr));
-     printingfunction(floatArr,sizeof(floatArr));
-     printingfunction(doubleArr,sizeof(doubleArr));
-     printingfunction(charArr,sizeof(charArr));
-    return 0;
+//to print any array by taking the array, its size and datatype
+void printAnyArray(void* arr, int size, char type){
+	int i;
+	//to print the array of different datatypes
+    switch(type){
+    	case 'i' :
+    		for(i = 0; i < size; i++){
+    			printf("%d ", *((int*) arr + i));
+			}
+			printf("\n");
+			break;
+			
+		case 'c' :
+    		for(i = 0; i < size; i++){
+    			printf("%c ", *((char*) arr + i));
+			}
+			printf("\n");
+			break;
+		
+		case 'f' :
+    		for(i = 0; i < size; i++){
+    			printf("%f ", *((float*) arr + i));
+			}
+			printf("\n");
+			break;
+		
+		case 'd' :
+    		for(i = 0; i < size; i++){
+    			printf("%f ", *((double*) arr + i));
+			}
+			printf("\n");
+			break;
+		
+		case 'l' :
+    		for(i = 0; i < size; i++){
+    			printf("%d ", *((unsigned long*) arr + i));
+			}
+			printf("\n");
+			break;
+			
+	}
 }
+
+void main(){
+	
+	int intArray[10] = {0};
+	char charArray[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+	float floatArray[10] = {0};
+	double doubleArray[10] = {0};
+	unsigned long ulArray[10] = {0};
+
+	//calling function for different cases
+	printAnyArray(intArray, 10, 'i');
+	printAnyArray(charArray, 10, 'c');
+	printAnyArray(floatArray, 10, 'f');
+	printAnyArray(doubleArray, 10, 'd');
+	printAnyArray(ulArray, 10, 'l');
+	
+} //end main
