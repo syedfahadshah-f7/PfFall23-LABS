83197c1ba2dcafe1b5ef7b65371bce3eb1bd75df
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-12-02 22:24:42 +0500

    Update task2.c

--- lab 12/task2.c
+++ lab 12/task2.c
@@ -1,42 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>>
+#include<stdio.h>
+#include<stdbool.h>
 
-int compare_files(FILE *fptr1, FILE *fptr2) {
-    unsigned long pos;
-    int c1, c2;
-    for (pos = 0;; pos++) {
-        c1 = getc(fptr1);
-        c2 = getc(fptr2);
-        if (c1 != c2 || c1 == EOF)
-            break;
-    }
-    if (c1 == c2) {
-        printf("files are identical and have %lu bytes\n", pos);
-        return 0;  // files are identical
-    } else
-    if (c1 == EOF) {
-        printf("file1 is included in file2, the first %lu bytes are identical\n", pos);
+int main() {
+    FILE *a = fopen("test1.txt", "rb");
+    FILE *b = fopen("test2.txt", "rb");
+
+    if (a == NULL || b == NULL) {
+        perror("Error opening files");
         return 1;
-    } else
-    if (c2 == EOF) {
-        printf("file2 is included in file1, the first %lu bytes are identical\n", pos);
-        return 2;
-    } else {
-        printf("file1 and file2 differ at position %lu: 0x%02X <> 0x%02X\n", pos, c1, c2);
-        return 3;
     }
-}
 
-int main()
-{
+    bool flag = true;
 
-FILE* fptr1;
-FILE* fptr2;
-fptr1= fopen("t1.docx", "w");
-fptr2=fopen("t2.docx","w");
-fprintf(fptr1,"%s %s %s %s","this","is","a","test");
-fprintf(fptr2,"%s %s %s %s","this","is","a","test");
-fptr1= fopen("t1.docx", "rb");
-fptr2=fopen("t2.docx","rb");
-compare_files(fptr1,fptr2);
+    while (1) {
+        int c1 = fgetc(a);
+        int c2 = fgetc(b);
+
+        if (c1 != c2) {
+            flag = false;
+            break;
+        }
+
+        if (c1 == EOF || c2 == EOF) {
+            break;
+        }
+    }
+
+    if (flag) {
+        printf("Equal\n");
+    } else {
+        printf("Not Equal\n");
+    }
+
+    fclose(a);
+    fclose(b);
+
+    return 0;
 }
